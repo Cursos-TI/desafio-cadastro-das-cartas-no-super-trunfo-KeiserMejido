c50e7dc20964283fe7951c99f8340201db506b35
KeiserMejido <KeiserMejido@gmail.com>
2025-06-24 12:12:27 +0000

    teste 1 add the card 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 
 int main() {
     
-    char state, cod[2], city[20] ; 
+    char state, cod[3], city[20] ; 
     // state: letter that represents the state (e.g., 'T' for Tocantins)
     // cod: two-character code that represents each card
     int population, places ;
@@ -12,19 +12,21 @@ int main() {
     //size: area of the city in square kilometers (km²)
 
  
+
+    //this section collects the card 1 infos 
     printf (" Wellcome to Super Trunfo!\n\n");
     printf (" Add the informations for card number 1\n\n");
     printf ("enter the initial of the state:\n");
-    scanf ("%c", &state);
+    scanf (" %c", &state);
 
     printf ("Set the card code:\n");
     scanf ("%s", &cod);
 
-    printf ("what's the city name?:\n");
-    scanf ("%s", &city);
-
     printf ("what's the population?:\n");
     scanf ("%d", &population);
+
+    printf ("what's the city name?:\n");
+    scanf ("%s", &city);
 
     printf ("what's the area of the city?:\n");
     scanf ("%f", &size);
@@ -36,12 +38,43 @@ int main() {
     scanf ("%d", &places);
 
 
-    return 0 
+
+    //shows the card 1 
+    printf ("\n Card number 1:\n");
+    printf ("state:%c \n", state);
+    printf ("code: %c%s \n", state, cod);
+    printf ("city name:%s \n", city);
+    printf ("population is: %d \n", population);
+    printf ("Area is: %f km² \n", size);
+    printf ("the PIB is: R$%f2 \n", PIB);
+    printf ("tourist attractions: %d \n", places);
 
 
 
 
+ //this section collects the card 2 infos 
 
+    ///printf (" Add the informations for card number 2\n\n");
+    ///printf ("enter the initial of the state:\n");
+    ///scanf ("%c", &state);
+
+   /// printf ("Set the card code:\n");
+    ///scanf ("%s", &cod);
+
+   /// printf ("what's the city name?:\n");
+    ///scanf ("%s", &city);
+
+   /// printf ("what's the population?:\n");
+   /// scanf ("%d", &population);
+
+    ///printf ("what's the area of the city?:\n");
+    ///scanf ("%f", &size);
+
+    ///printf ("what's the PIB of the city?:\n ");
+    ///scanf ("%f", &PIB);
+
+    ///printf ("How many tourist attractions?:\n");
+   /// scanf ("%d", &places);
 
 
 
