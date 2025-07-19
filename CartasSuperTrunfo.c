f711d939db966503492d2a07921e4de76d1263b7
KeiserMejido <KeiserMejido@gmail.com>
2025-07-19 22:29:06 +0000

    adiciona o nivel mestre, incrementando as comparações das cartas com a winner section

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,23 +3,27 @@
 
 int main() {
     
-    //the variants numbered with 2, correspond to card 2
+//the variants numbered with 2, correspond to card 2
+    
 
     char state, cod[3], city[20], state2, cod2[3], city2[20];
-    // state: letter that represents the state (e.g., 'T' for Tocantins)
-    // cod: two-character code that represents each card
-    int population, places, population2, places2;
-    // places: number of tourist attractions in the city
-    float size, PIB, size2, PIB2; 
-    //size: area of the city in square kilometers (km²)
-    //PIB: Gross Domestic Product of the city in Brazilian Reals (R$)
+// state: letter that represents the state (e.g., 'T' for Tocantins)
+// cod: two-character code that represents each card
+    int places, places2, population_winner, size_winner, PIB_winner, places_winner, PIB_per_winner, pop_density_winner, SuperPower_winner ;
+// places: number of tourist attractions in the city
+    unsigned long int population, population2;
+//SuperPower: the strength of the card, calculated by summing all its numerical attributes.
+    float size, size2; 
+    double PIB, PIB2, SuperPower, SuperPower2 ;
+//size: area of the city in square kilometers (km²)
+//PIB: Gross Domestic Product of the city in Brazilian Reals (R$)
     float PIB_per, pop_density, PIB_per2, pop_density2 ;
-    // PIB_per: GDP per capita, average amount of money each person
-    // pop_density: population density of the city (people per km²)
+// PIB_per: GDP per capita, average amount of money each person
+// pop_density: population density of the city (people per km²)
     
     
 
-    //this section collects the card 1 infos 
+//this section collects the card 1 infos 
     printf (" Wellcome to Super Trunfo!\n\n");
     printf (" Add the informations for card number 1\n\n");
     printf ("enter the initial of the state:\n");
@@ -29,7 +33,7 @@ int main() {
     scanf ("%s", &cod);
 
     printf ("what's the population?:\n");
-    scanf ("%d", &population);
+    scanf ("%lu", &population);
 
     printf ("what's the city name?:\n");
     scanf ("%s", &city);
@@ -38,18 +42,18 @@ int main() {
     scanf ("%f", &size);
 
     printf ("what's the PIB of the city?:\n ");
-    scanf ("%f", &PIB);
+    scanf ("%lf", &PIB);
 
     printf ("How many tourist attractions?:\n");
     scanf ("%d", &places);
 
-    //calculates the GDP and population density for card 1
+//calculates the GDP and population density for card 1
     PIB_per = PIB / population;
-    //calculates the population density of the city for card 1
-    pop_density = population / size;
+//calculates the population density of the city for card 1
+    pop_density = (float) population / size;
 
-
-
+    SuperPower = (float)population + size + PIB + (float)places + PIB_per + (1.0 / pop_density);
+    
     
  //this section collects the card 2 infos 
     printf (" Add the informations for card number 2\n\n");
@@ -60,7 +64,7 @@ int main() {
     scanf ("%s", &cod2);
 
     printf ("what's the population?:\n");
-    scanf ("%d", &population2);
+    scanf ("%lu", &population2);
 
     printf ("what's the city name?:\n");
     scanf ("%s", &city2);
@@ -69,16 +73,17 @@ int main() {
     scanf ("%f", &size2);
 
     printf ("what's the PIB of the city?:\n ");
-    scanf ("%f", &PIB2);
+    scanf ("%lf", &PIB2);
 
     printf ("How many tourist attractions?:\n");
     scanf ("%d", &places2);
 
     //calculates the GDP and population density for card 2
-    PIB_per2 = PIB / population;
+    PIB_per2 = PIB2 / population2;
     //calculates the population density of the city for card 2
-    pop_density2 = population / size;
+    pop_density2 = (float) population2 / size2;
 
+    SuperPower2 = (float)population2 + size2 + PIB2 + (float)places2 + PIB_per2 + (1.0 / pop_density2);
 
 //shows the card 1 
     printf ("\n Card number 1:\n");
@@ -91,6 +96,7 @@ int main() {
     printf ("tourist attractions: %d \n", places);
     printf ("PIB per Capita is R$%.2f per person \n", PIB_per);
     printf ("Population Density is %.2f person per km² \n", pop_density);
+    printf ("The Super Power is %.2f \n", SuperPower );
 
 //shows the card 2
     printf ("\n Card number 2:\n");
@@ -103,7 +109,30 @@ int main() {
     printf ("tourist attractions: %d \n", places2);
     printf ("PIB per Capita is R$%.2f per person \n", PIB_per2);
     printf ("Population Density is %.2f person per km² \n", pop_density2);
+    printf ("The Super Power is %.2f \n", SuperPower2 );
+
+//winner section
+    population_winner = population > population2;
+    size_winner = size > size2;
+    PIB_winner = PIB > PIB2; 
+    places_winner = places > places2;
+    PIB_per_winner = PIB_per > PIB_per2;
+    pop_density_winner = pop_density < pop_density2;
+    SuperPower_winner = SuperPower > SuperPower2;
 
 
-    return 0;
+   printf("\n WINNER SECTION \n");
+   printf("(1) means Card 1 won — (0) means Card 2 won.\n\n");
+
+   printf("Population: %d\n", population_winner);
+   printf("Largest Area: %d\n", size_winner);
+   printf("Highest GDP (PIB): %d\n", PIB_winner);
+   printf("Most Tourist Attractions: %d\n", places_winner);
+   printf("Best GDP per Capita: %d\n", PIB_per_winner);
+   printf("Lowest Population Density: %d\n", pop_density_winner);
+   printf("\nSuper Power Clash: %d\n", SuperPower_winner);
+
+
+
+ return 0;
 }
